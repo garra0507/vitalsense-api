b29760173b8842be886ed78af74c62138e55c7a0
Alumnos <alumnos@h51-12.upc.edu.pe>
2026-05-05 21:53:02 -0500

    first commit

---
+++ src/test/java/com/biotech/vitalsense/VitalsenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biotech.vitalsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalsenseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
