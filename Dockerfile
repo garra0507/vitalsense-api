a4f7c65e2b761353fc97b0a65e46a8b26b89ba64
jojande <jojande2005@gmail.com>
2026-05-11 18:43:45 -0500

    feat: enable Swagger/OpenAPI and fix Docker/prod configs for Render deployment

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM eclipse-temurin:21-jdk-alpine
 WORKDIR /app
-COPY target/vitalsense-0.0.1-SNAPSHOT.jat app.jar
+COPY target/vitalsense-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-java","app.jar"]
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/biotech/vitalsenseapi/config/SecurityConfig.java
+++ src/main/java/com/biotech/vitalsenseapi/config/SecurityConfig.java
@@ -28,7 +28,13 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/api/doctors/**",
                                 "/api/availability/**",
-                                "/api/appointments/**"
+                                "/api/appointments/**",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**"
                         )
                         .permitAll()
                         .anyRequest()
