8c2868be9a8b79c4ff93c4071d96611a319af55e
jojande <jojande2005@gmail.com>
2026-05-12 17:55:05 -0500

    fix: disable httpBasic to prevent browser popups and enforce JWT

--- src/main/java/com/biotech/vitalsenseapi/config/SecurityConfig.java
+++ src/main/java/com/biotech/vitalsenseapi/config/SecurityConfig.java
@@ -48,8 +48,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
